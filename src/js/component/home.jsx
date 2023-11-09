98cf9eb073be6742edbe1cd8cb9be3e59b9f832b
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2023-11-09 01:49:01 +0000

    Pronto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,37 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-
-//create your first component
 function Home() {
-	const [glowing, setGlowing] = useState('red');
-	const [purpleVisible, setPurpleVisible] = useState(false);
-	return (
-		<div className="text-center">
-				<button type="button" className="btn btn-primary" onClick={() => {
-					if (glowing === "red"){setGlowing("green")}
-					if (glowing === "green"){setGlowing("yellow")}
-					if (glowing === "yellow"){setGlowing("red")}}}>
-					Cambiar colores
-				</button>
-				<button type="button" className="btn btn-secondary m-3" onClick={() => setPurpleVisible(true)}>
-					Luz Púrpura
-				</button>
-		<div className="traffic">
-			<div className={"lamp red " + (glowing === "red" ? "glowingRed" : "redTenue")} onClick={() => setGlowing("red")}></div>
-			<div className={"lamp yellow " + (glowing === "yellow" ? "glowingYellow" : "yellowTenue")} onClick={() => setGlowing("yellow")}></div>
-			<div className={"lamp green " + (glowing === "green" ? "glowingGreen" : "greenTenue")} onClick={() => setGlowing("green")}></div>
-			{purpleVisible && (
+  const [glowing, setGlowing] = useState('red');
+  const [purpleVisible, setPurpleVisible] = useState(false);
+  const trafficClass = purpleVisible ? "trafficPurple" : "traffic";
+
+  const togglePurple = () => {
+    setPurpleVisible(!purpleVisible);
+    setGlowing('purple');
+  };
+
+  return (
+    <div className="text-center">
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          if (glowing === "purple") {
+            setGlowing("green");
+          } else if (glowing === "red") {
+            setGlowing("purple");
+          } else if (glowing === "green") {
+            setGlowing("yellow");
+          } else if (glowing === "yellow") {
+            setGlowing("red");
+          }
+        }}
+      >
+        Cambiar colores
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary m-3"
+        onClick={togglePurple}
+      >
+        Luz Púrpura
+      </button>
+      <div className={trafficClass}>
+	  {purpleVisible && (
           <div
             className={"lamp purple " + (glowing === "purple" ? "glowingPurple" : "")}
             onClick={() => setGlowing("purple")}
           ></div>
         )}
-			<div className="poste"></div>
-		</div>
+        <div
+          className={"lamp red " + (glowing === "red" ? "glowingRed" : "redTenue")}
+          onClick={() => setGlowing("red")}
+        ></div>
+        <div
+          className={"lamp yellow " + (glowing === "yellow" ? "glowingYellow" : "yellowTenue")}
+          onClick={() => setGlowing("yellow")}
+        ></div>
+        <div
+          className={"lamp green " + (glowing === "green" ? "glowingGreen" : "greenTenue")}
+          onClick={() => setGlowing("green")}
+        ></div>
+        
+      </div>
+      <div className="text-center poste"></div>
+    </div>
+  );
+}
 
-		</div>
-	
-
-	);
-};
 export default Home;
